d291f0f3753dca975bfa6420eb4a1da48070e9e5
johnson yuen <j@johnsonyuen.com>
2023-12-23 11:00:40 +0800

    Add 2 queries

--- query.py
+++ query.py
@@ -7,12 +7,23 @@ db = Session()
 db.query("create database db")
 db.query("use db")
 
+# Create table
 create_table_sql = "create table crime Engine=Log as select * FROM file('crime.parquet')"
 res = db.query(create_table_sql, "Pretty")
-query_sql = "select * FROM crime limit 2"
+
+# Query 1
+query_sql = "describe table crime"
 res = db.query(query_sql, "Pretty")
+print(res)
+print(
+    f"{res.rows_read()} rows | "
+    f"{res.bytes_read()} bytes | "
+    f"{res.elapsed()} seconds"
+)
 
-
+# Query 2
+query_sql = "select count(LOCATION) as num, LOCATION FROM crime group by LOCATION order by num DESC"
+res = db.query(query_sql, "Pretty")
 print(res)
 print(
     f"{res.rows_read()} rows | "
